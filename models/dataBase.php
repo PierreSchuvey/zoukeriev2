adc4708f6da811a1d01b138e50d20fe1620d4307
PierreSchuvey <pierre.schuveye2n@gmail.com>
2021-08-19 15:48:44 +0200

    v2

--- models/dataBase.php
+++ models/dataBase.php
@@ -3,11 +3,6 @@
 class dataBase {
 
     //L'attribut $db sera disponible dans ses enfants
-    $cleardb_url      = parse_url("mysql://b4989a81bc327b:09addbf9@eu-cdbr-west-01.cleardb.com/heroku_ef333d26a4fcbf4?reconnect=true");
-    $cleardb_server   = $cleardb_url["host"];
-    $cleardb_username = $cleardb_url["user"];
-    $cleardb_password = $cleardb_url["pass"];
-    $cleardb_db       = substr($cleardb_url["path"],1);
     protected $db;
 
     /*
@@ -17,6 +12,11 @@ class dataBase {
 
     public function __construct() {
         try {
+            $cleardb_url      = parse_url(getenv("CLEARDB_DATABASE_URL"));
+            $cleardb_server   = $cleardb_url["host"];
+            $cleardb_username = $cleardb_url["user"];
+            $cleardb_password = $cleardb_url["pass"];
+            $cleardb_db       = substr($cleardb_url["path"],1);
             $this->db = new PDO('mysql:host=eu-cdbr-west-01.cleardb.com;dbname=`heroku_ef333d26a4fcbf4`;charset=utf8', 'b4989a81bc327b', '09addbf9');
         } catch (Exception $e) {
             die('Erreur : ' . $e->getMessage());
