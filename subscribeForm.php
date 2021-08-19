9f8af9eca71c80560beaaa00281416b330dbda1e
PierreSchuvey <pierre.schuveye2n@gmail.com>
2021-08-19 17:03:40 +0200

    v2

--- subscribeForm.php
+++ subscribeForm.php
@@ -18,28 +18,28 @@ include_once 'controllers/addUserController.php';
 
         </div>
         <form id="subForm" action="" method="post" enctype="multipart/form-data">
-            <div class="col-lg-offset-4 col-lg-4  topLog">
+            <div class=" offset-1 col-10 offset-sm-3 col-sm-6  topLog">
                 <label>Pseudo* : </label>
             </div>
-            <div class="form-group col-lg-offset-4 col-lg-4">
+            <div class="form-group  offset-1 col-10 offset-sm-3 col-sm-6">
                 <input type="text" name="pseudo" class="form-control" value="<?= isset($_POST['pseudo']) ? $_POST['pseudo'] : '' ?>" >
                 <p class = "errorsLoginSub"><?= isset($formError['pseudo']) ? $formError['pseudo'] : '' ?></p>
             </div>
-            <div class="form-group col-lg-offset-4 col-lg-4">
+            <div class="form-group  offset-1 col-10 offset-sm-3 col-sm-6">
                 <label>Mot de passe* : </label>
             </div>
-            <div class="form-group col-lg-offset-4 col-lg-4">
+            <div class="form-group  offset-1 col-10 offset-sm-3 col-sm-6">
                 <input type="password" name="password" class="form-control" >
                 <p class="errorsLoginSub"><?= isset($formError['password']) ? $formError['password'] : '' ?></p>
             </div>
-            <div class="form-group col-lg-offset-4 col-lg-5">
+            <div class="form-group  offset-1 col-10 offset-sm-3 col-sm-6">
                 <label>Mot de passe (confirmation)* : </label>
             </div>
-            <div class="form-group col-lg-offset-4 col-lg-4">
+            <div class="form-group  offset-1 col-10 offset-sm-3 col-sm-6">
                 <input type="password" name="confPassword" class="form-control" >
                 <p class="errorsLoginSub"><?= isset($formError['confPassword']) ? $formError['confPassword'] : '' ?></p>
             </div>
-            <div class="form-group form-group col-lg-offset-8 col-lg-2">
+            <div class="form-group form-group  offset-1 col-10 offset-sm-3 col-sm-6">
                 <button type="submit" class="form-control" name="validInscrip"> Valider l'inscription</button>
             </div>
         </form>
