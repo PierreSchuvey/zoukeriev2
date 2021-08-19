0abb69ca43db0947e5b3cb2117eb9a38a03bf25e
PierreSchuvey <pierre.schuveye2n@gmail.com>
2021-08-19 15:42:02 +0200

    v2

--- models/dataBase.php
+++ models/dataBase.php
@@ -3,6 +3,11 @@
 class dataBase {
 
     //L'attribut $db sera disponible dans ses enfants
+    $cleardb_url      = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $cleardb_server   = $cleardb_url["host"];
+    $cleardb_username = $cleardb_url["user"];
+    $cleardb_password = $cleardb_url["pass"];
+    $cleardb_db       = substr($cleardb_url["path"],1);
     protected $db;
 
     /*
