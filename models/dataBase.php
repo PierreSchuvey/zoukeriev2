f9166bef7c1742d2e62da1cae8b44803a239153f
PierreSchuvey <pierre.schuveye2n@gmail.com>
2021-08-19 14:15:46 +0200

    v2

--- models/dataBase.php
+++ models/dataBase.php
@@ -12,7 +12,7 @@ class dataBase {
 
     public function __construct() {
         try {
-            $this->db = new PDO('InnoDB:host=eu-cdbr-west-01.cleardb.com;dbname=`heroku_ef333d26a4fcbf4`;charset=utf8', 'b4989a81bc327b', '09addbf9');
+            $this->db = new PDO('mysql:host=eu-cdbr-west-01.cleardb.com;dbname=`heroku_ef333d26a4fcbf4`;charset=utf8', 'b4989a81bc327b', '09addbf9');
         } catch (Exception $e) {
             die('Erreur : ' . $e->getMessage());
         }
