b39b2b2e1ab4dd0253492e123ccbdf349da823b8
PierreSchuvey <pierre.schuveye2n@gmail.com>
2021-08-19 16:43:52 +0200

    v2

--- vues/header.php
+++ vues/header.php
@@ -5,6 +5,7 @@
     <title>La Zoukerie</title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, minimum-scale=1.0">
     <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.css" />
     <link rel="stylesheet" href="assets/css/style.css" />
     <link href="https://fonts.googleapis.com/css?family=Cabin" rel="stylesheet">
